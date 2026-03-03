1233e22c4cb9300ca5f5573b774b0c4e37e035d9
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2026-03-03 18:49:58 +0530

    Build a various templates for different style of documents

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"; // Space fixed!
+"use client"; 
 
 import Link from "next/link";
 import Image from "next/image";
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
+"use client"; 
+
 import { Navbar } from "./navbar"
+import { TemplateGallery } from "./TemplateGallery"
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
         <Navbar />
+      </div>
+      <div className="mt-16">
+          <TemplateGallery />
       </div>
     </div>
   )
